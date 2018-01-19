830c1fa8e736eea849466228618e257e37c8f259
Umar Fadil <umarfadil.skom@gmail.com>
2018-01-19 18:50:33 +0700

    First Commit

---
+++ app/src/main/java/dev/nullpointer/bottomsheet/BottomSheetFragment.java
@@ -0,0 +1,31 @@
+package dev.nullpointer.bottomsheet;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by umarfadil on 1/19/18.
+ * website : www.umarfadil.com
+ * github : https://github.com/umarfadil
+ */
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+    public BottomSheetFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false);
+    }
+}
